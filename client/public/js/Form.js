4aa3de8674c53fb0f5b1a530a1951f5cb3513e3b
Orsinet <frantz.o@icloud.com>
2025-04-17 13:40:41 +0200

    chore(form): add form script

---
+++ client/public/js/Form.js
@@ -0,0 +1,13 @@
+// Fonction pour récupérer la liste des contrats
+
+function fetchContrats() {
+  fetch('http://localhost:3001/contrat')
+    .then(response => response.json)
+    .then(data => {
+      console.log('Contrats récupérés : ', data);
+    });
+
+  // const contratSelect = document.getElementById('contrat');
+}
+
+fetchContrats();
