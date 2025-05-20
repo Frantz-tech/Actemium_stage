55d43818e95f9d3faa897e857263bfb767d46f6a
Orsinet <frantz.o@icloud.com>
2025-05-20 11:22:40 +0200

    chore(client): add css for modal user and fix sign in redirection

--- client/public/js/signIn.js
+++ client/public/js/signIn.js
@@ -18,7 +18,7 @@ loginForm.addEventListener('submit', async e => {
   }
 
   try {
-    const response = await fetch('http://localhost:3000/admin/login', {
+    const response = await fetch('http://localhost:3000/api/admin/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
@@ -27,15 +27,23 @@ loginForm.addEventListener('submit', async e => {
     const data = await response.json();
 
     if (response.ok) {
-      localStorage.setItem('token', data.token);
+      localStorage.setItem('token', data.data.token);
       alert('Connexion réussie !');
 
       console.log(data);
       // Redirection vers la page d'accueil admin (à adapter)
-      if (data.ROLE === 'admin') {
-        window.location.href = '../pages/adminDashboard.html';
+      if (data.data.user && data.data.user.ROLE) {
+        const role = data.data.user.ROLE.trim().toLowerCase();
+        console.log('Role détecté:', role);
+        if (role === 'administrateur') {
+          window.location.href = '../pages/adminDashboard.html';
+          alert('redirection vers la page admin');
+        } else {
+          window.location.href = '../pages/accueil.html';
+          alert("redirection vers la page d'accueil");
+        }
       } else {
-        window.location.href = '../pages/accueil.html';
+        console.log('Aucun rôle détecté dans data.data.user');
       }
     } else {
       alert('Erreur : ' + data.message);
