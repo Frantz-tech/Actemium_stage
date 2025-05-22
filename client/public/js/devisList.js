0aff9e1318300cd0f104c2e5fbe89d488b4d0e5b
Orsinet <frantz.o@icloud.com>
2025-05-22 16:42:45 +0200

    chore(client): changing devisList.js so everything has less html and insert js

--- client/public/js/devisList.js
+++ client/public/js/devisList.js
@@ -1,171 +1,175 @@
 document.querySelector('h1').innerText = 'DEVIS';
 
-const btnCreerDevis = document.querySelector('.btnCreer');
-btnCreerDevis.innerText = 'Créer un devis';
+// Charger les devis lorsque l'on arrive sur la page liste des devis :
+// Fetch la route get devis par RA_ID
+function fetchDevisByRaId() {
+  fetch('http://localhost:3000/api/devis')
+    .then(response => response.json())
+    .then(data => {
+      console.log('Devis récupérés :', data);
+      const devis = data.data;
 
-const btnGoToPostModal = document.querySelector('.goToPostBtn');
-btnGoToPostModal.addEventListener('click', () => {
-  window.location.href = '../pages/postesList.html';
-});
+      if (Array.isArray(devis) && devis.length > 0) {
+        const grpDevisList = document.createElement('div');
+        grpDevisList.id = 'groupeDevisList';
 
-btnCreerDevis.addEventListener('click', () => {
-  alert('Création d un nouveau devis');
-  window.location.href = '../pages/devis.html';
-});
+        const contenuDevisList = document.createElement('div');
+        contenuDevisList.classList.add('contenuDevisList');
 
-// Chargement des devis depuis le fichier JSON local pour test
+        const devisList = document.createElement('div');
+        devisList.id = 'devisList';
+        devis.forEach(d => {
+          // Contenu du devis
+          const devisItem = document.createElement('div');
+          devisItem.classList.add('devisItem');
 
-fetch('../js/devisList.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log('JSON local chargé :', data);
+          // Libelle et ref du devis
+          const libelleRef = document.createElement('div');
+          libelleRef.classList.add('libelleRef');
 
-    const devisList = document.getElementById('devisList');
-    devisList.innerHTML = '';
+          //Libelle du devis
+          const libelle = document.createElement('p');
+          libelle.classList.add('devisLibelle');
+          libelle.textContent = d.LIBELLE;
 
-    data.forEach(devi => {
-      const groupeA = document.createElement('div');
-      groupeA.classList.add('groupeLibelle');
+          //Devis_ref
+          const devis_ref = document.createElement('p');
+          devis_ref.classList.add('devis_ref');
+          devis_ref.textContent = d.DEVIS_REF;
 
-      // Contenu du devis
-      const devisItem = document.createElement('div');
-      devisItem.innerHTML = `<p>Libellé : ${devi.libelle}</p> <p>N° : ${devi.code_codex}</p>`;
-      devisItem.classList.add('devisItem');
+          // Bouton aller page liste des postes
+          const btnGoToPost = document.createElement('button');
+          btnGoToPost.textContent = '⎘';
+          btnGoToPost.classList.add('btnGoToPost');
 
-      // Bouton aller page liste des postes
-      const btnGoToFap = document.createElement('div');
-      btnGoToFap.textContent = '⎘';
-      btnGoToFap.classList.add('btnGoToPost');
-      btnGoToFap.addEventListener('click', () => {
-        window.location.href = '../pages/fap.html';
-      });
+          btnGoToPost.addEventListener('click', () => {
+            alert('redirection vers la liste des postes associés a ce devis');
+            // window.location.href = '../pages/postesList.html';
+          });
 
-      // Supprimer un devis
-      const deleteDevis = document.createElement('div');
-      deleteDevis.classList.add('deletebtn');
+          // Supprimer un devis
+          const btnDeleteDevis = document.createElement('button');
+          btnDeleteDevis.classList.add('deletebtn');
 
-      deleteDevis.addEventListener('click', () => {
-        const confirmation = confirm(
-          `Êtes-vous sûr de vouloir supprimer le devis ${devi.code_codex}  ? Cette action est irréversible.`
-        ); // Confirmation pour supprimer un devis, a faire également pour les postes dans le js postesListes
-        if (confirmation) {
-          const removed = devisList.removeChild(groupeA);
-          console.log('Élément supprimé :', removed);
-        }
-      });
+          btnDeleteDevis.addEventListener('click', () => {
+            const confirmation = confirm(
+              `Êtes-vous sûr de vouloir supprimer le devis ? Cette action est irréversible.`
+            ); // Confirmation pour supprimer un devis, a faire également pour les postes dans le js postesListes
+            if (confirmation) {
+              const removed = devisList.removeChild(devisItem);
+              console.log('Élément supprimé :', removed);
+            }
+          });
 
-      devisList.appendChild(groupeA);
-      groupeA.appendChild(devisItem);
-      groupeA.appendChild(deleteDevis);
-      groupeA.appendChild(btnGoToFap);
-
-      /* -- Action lors du clique sur le devis -- */
-
-      devisItem.addEventListener('click', () => {
-        const modal = document.getElementById('modal');
-        const modalBody = document.getElementById('modal_body');
-        const closeBtn = document.querySelector('.close-btn');
-        modalBody.innerHTML = '';
-
-        //Libelle du devis
-        const devisLibelle = document.createElement('div');
-        devisLibelle.textContent = `${devi.libelle}`;
-        devisLibelle.classList.add('modalDevisLibelle');
-        modalBody.appendChild(devisLibelle);
-
-        // Div pour regrouper la segmentation
-        const groupSegm = document.createElement('div');
-        groupSegm.classList.add('groupSegm');
-
-        // Groupe de la segmentation et le N°Codex + RA_ID
-        const groupSegmCodexRa = document.createElement('div');
-        groupSegmCodexRa.classList.add('groupeSegmCodexRa');
-        groupSegmCodexRa.appendChild(groupSegm);
-        modalBody.appendChild(groupSegmCodexRa);
-
-        // N°Codex RA_ID Etat
-        const devisCodexRa = document.createElement('div');
-        devisCodexRa.textContent = `${devi.code_codex} - ${devi.RA_id} - ETAT : ${devi.etat}`;
-        devisCodexRa.classList.add('modalDevisCodexRa');
-        groupSegmCodexRa.appendChild(devisCodexRa);
-
-        // Commanditaire
-        const devisCommanditaire = document.createElement('div');
-        devisCommanditaire.textContent = `${devi.commanditaire}`;
-        devisCommanditaire.classList.add('modalCommanditaire');
-        groupSegm.appendChild(devisCommanditaire);
-
-        // Client
-        const devisClient = document.createElement('div');
-        devisClient.textContent = `${devi.client}`;
-        devisClient.classList.add('modalSegm');
-        groupSegm.appendChild(devisClient);
-
-        // Expertise
-        const devisExpertise = document.createElement('div');
-        devisExpertise.textContent = `${devi.expertise}`;
-        devisExpertise.classList.add('modalSegm');
-        groupSegm.appendChild(devisExpertise);
-
-        // Domaine
-        const devisDomaine = document.createElement('div');
-        devisDomaine.textContent = `${devi.domaine}`;
-        devisDomaine.classList.add('modalSegm');
-        groupSegm.appendChild(devisDomaine);
-
-        // Contrat
-        const devisContrat = document.createElement('div');
-        devisContrat.textContent = `${devi.contrat}`;
-        devisContrat.classList.add('modalSegm');
-        groupSegm.appendChild(devisContrat);
-
-        // Modifier le devis
-        const btnModal = document.querySelector('.btnModal');
-        btnModal.innerText = 'Modifier';
-        btnModal.addEventListener('click', () => {
-          alert('Modifier le devis ');
+          devisList.appendChild(devisItem);
+          devisItem.append(libelleRef, btnGoToPost, btnDeleteDevis);
+          libelleRef.append(libelle, devis_ref);
         });
 
-        // Modal action
-        modal.classList.remove('hidden');
-        document.body.classList.add('noscroll');
-        closeBtn.onclick = () => {
-          modal.classList.add('hidden');
-          document.body.classList.remove('noscroll');
-        };
+        contenuDevisList.appendChild(devisList);
+        grpDevisList.appendChild(contenuDevisList);
+        const main = document.querySelector('main');
+        main.appendChild(grpDevisList);
+      }
+
+      const btnCreerDevis = document.createElement('button');
+      btnCreerDevis.classList.add('btnCreer');
+      btnCreerDevis.textContent = 'Créer un devis';
+      btnCreerDevis.addEventListener('click', () => {
+        alert('Création d un nouveau devis');
+        window.location.href = '../pages/devis.html';
       });
+
+      const main = document.querySelector('main');
+      main.append(btnCreerDevis);
     });
-  })
-  .catch(error => {
-    console.error('Erreur lors du chargement du JSON local :', error);
+}
+
+/* -- Action lors du clique sur le devis -- */
+const devisItem = document.createElement('div');
+devisItem.classList.add('devisItem');
+
+devisItem.addEventListener('click', e => {
+  e.preventDefault();
+  // devisItem.addEventListener suite ..
+  const modal = document.createElement('div');
+  modal.classList.add('modalUser');
+  document.body.classList.add('noscroll');
+  modal.style.display = 'flex';
+  modal.classList.add('show');
+  const closeBtn = document.createElement('button').classList.add('close-btn');
+
+  const overlay = document.createElement('div');
+  overlay.classList.add('modalDevis-overlay');
+
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modalDevis_content');
+
+  //Libelle du devis
+  const devisLibelle = document.createElement('div');
+  devisLibelle.textContent = ``;
+  devisLibelle.classList.add('modalDevisLibelle');
+
+  // Groupe de la segmentation et le N°Codex + RA_ID
+  const groupSegmCodexRa = document.createElement('div');
+  groupSegmCodexRa.classList.add('groupeSegmCodexRa');
+  groupSegmCodexRa.appendChild(groupSegm);
+
+  // Div pour regrouper la segmentation
+  const groupSegm = document.createElement('div');
+  groupSegm.classList.add('groupSegm');
+
+  // N°Codex RA_ID Etat
+  const devisCodexRa = document.createElement('div');
+  devisCodexRa.textContent = ``;
+  devisCodexRa.classList.add('modalDevisCodexRa');
+
+  // Commanditaire
+  const devisCommanditaire = document.createElement('div');
+  devisCommanditaire.textContent = ``;
+  devisCommanditaire.classList.add('modalCommanditaire');
+
+  // Client
+  const devisClient = document.createElement('div');
+  devisClient.textContent = ``;
+  devisClient.classList.add('modalSegm');
+
+  // Expertise
+  const devisExpertise = document.createElement('div');
+  devisExpertise.textContent = ``;
+  devisExpertise.classList.add('modalSegm');
+
+  // Domaine
+  const devisDomaine = document.createElement('div');
+  devisDomaine.textContent = ``;
+  devisDomaine.classList.add('modalSegm');
+
+  // Contrat
+  const devisContrat = document.createElement('div');
+  devisContrat.textContent = ``;
+  devisContrat.classList.add('modalSegm');
+
+  // Modifier le devis
+  const btnModifier = document.createElement('button').classList.add('btnModal');
+  btnModifier.innerText = 'Modifier';
+  btnModifier.addEventListener('click', () => {
+    alert('Modifier le devis ');
   });
 
-// fonction pour récupérer la liste des devis
+  // AppendChild && Append
+  overlay.appendChild(modal);
+  modal.appendChild(modalContent);
+  modalContent.append(devisLibelle, groupSegmCodexRa);
+  groupSegmCodexRa.appendChild(devisCodexRa);
+  groupSegm.append(devisCommanditaire, devisClient, devisExpertise, devisDomaine, devisContrat);
 
-// function fetchDevis() {
-//   fetch('route get de api/devis')
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log('fetchDevis | Devis récupérés : ', data);
+  // Modal action
+  modal.classList.remove('hidden');
+  document.body.classList.add('noscroll');
+  closeBtn.onclick = () => {
+    modal.classList.add('hidden');
+    document.body.classList.remove('noscroll');
+  };
+});
 
-//       const devisList = document.getElementById('devisList');
-//       const devis = data.data;
-//       devisList.textContent = ''; // A voir car normalement il faut mettre innerhtml pour que cela vide le contenu
-
-//       if (Array.isArray(devis) && devis.length > 0) {
-//         devis.forEach(devi => {
-//           const devisItem = document.createElement('div');
-//           devisItem.innerHTML = `<p>Libellé : ${devi.libelle}</p> <p>N° Codex : ${devi.code_codex}</p>`;
-//           devisList.appendChild(devisItem);
-//           devisItem.classList.add('devisItem');
-//         });
-//       } else {
-//         devisList.innerHTML = '<p> Aucun devis trouvé </p>';
-//       }
-//     })
-//     .catch(error => {
-//       console.error('Erreur lors de la récupération des devis : ', error);
-//       const devisList = document.getElementById('devisList');
-//       devisList.innerHTML = ' <p> Erreur lors de la récupération des devis </p>';
-//     });
-// }
+fetchDevisByRaId();
